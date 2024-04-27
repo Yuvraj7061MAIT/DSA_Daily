e97acb940be34049c38dea968757fdc608f7e19f
Yuvraj Singh <154665049+Yuvraj7061MAIT@users.noreply.github.com>
2024-04-27 09:32:26 +0530

    Time: 9 ms (93.98%), Space: 57.2 MB (15.23%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,70 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head; // Return the list if it has 0 or 1 node
+        }
+        
+        // Find the middle of the list
+        ListNode middle = findMiddle(head);
+        ListNode nextToMiddle = middle.next;
+        middle.next = null; // Split the list into two halves
+        
+        // Recursively sort the two halves
+        ListNode left = sortList(head);
+        ListNode right = sortList(nextToMiddle);
+        
+        // Merge the sorted halves
+        return merge(left, right);
+    }
+    
+    // Function to find the middle of the list using slow and fast pointers
+    private ListNode findMiddle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    
+    // Function to merge two sorted linked lists
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+        
+        // Connect the remaining nodes, if any
+        if (l1 != null) {
+            current.next = l1;
+        }
+        if (l2 != null) {
+            current.next = l2;
+        }
+        
+        return dummy.next;
+    }
+}
